5af939742578c0a2332ee6ccc738cb8d4694b5d6
David Orelowitz <david.orelowitz@gmail.com>
2018-08-10 22:00:03 +0100

    Initial commit

---
+++ src/test/java/com/david/ApplicationTest.java
@@ -0,0 +1,27 @@
+package com.david;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+public class ApplicationTest {
+
+  @Test
+  public void firstNonMockTest(){
+
+    User user = new User("David", "Orelowitz");
+    assertEquals("David Orelowitz", user.getFullName());
+  };
+
+  @Test
+  public void MockStubTest(){
+    User userMock = mock(User.class);
+    when(userMock.getFullName()).thenReturn("David Orelowitz");
+
+    assertEquals("David Orelowitz", userMock.getFullName());
+  }
+
+}
