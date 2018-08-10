8a5442d0129df4a232b89070e752a29198d0e795
David Orelowitz <david.orelowitz@gmail.com>
2018-08-10 17:56:49 -0400

    Added a couple of extra Mock cases

--- src/test/java/com/david/ApplicationTest.java
+++ src/test/java/com/david/ApplicationTest.java
@@ -3,8 +3,7 @@ package com.david;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
 public class ApplicationTest {
@@ -21,5 +20,14 @@ public class ApplicationTest {
     User userMock = mock(User.class);
     when(userMock.getFullName()).thenReturn("David Orelowitz");
     assertEquals("David Orelowitz", userMock.getFullName());
+
+    when(userMock.getFullName()).thenReturn("David O");
+    assertEquals("David O", userMock.getFullName());
+
+    when(userMock.getFullName()).thenReturn("David Orrie");
+    assertEquals("David Orrie", userMock.getFullName());
+
+    verify(userMock, times(3)).getFullName();
+    verify(userMock, times(0)).setFirstName("David");
   }
 }
